61b1e206480b7107c4b04daa298f6940e9774437
NurievLinar <nurievlu@yandex.ru>
2023-04-02 22:03:09 +0300

    Item. создал поля

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,4 +4,15 @@ package ru.practicum.shareit.item.model;
  * TODO Sprint add-controllers.
  */
 public class Item {
+
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private int owner;
+
+    private String available;
+
 }

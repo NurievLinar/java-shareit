2aab2904b4f03de0c3ca3457e0d53965307dd237
NurievLinar <nurievlu@yandex.ru>
2023-04-08 01:07:19 +0300

    ItemRequest.java : удалил неиспользуемые импорты.

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.user.User;
 
 import java.time.LocalDateTime;
